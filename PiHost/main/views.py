c46e761bae9ac6df287160880a397878a6c3b398
NIklas Ziermann <N-Ziermann@protonmail.com>
2019-05-19 17:54:12 +0200

    Webpanel functionality
    The webpanel can now change the buttons' commands.

--- PiHost/main/views.py
+++ PiHost/main/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Button
 
 # Create your views here.
 def homepage(request):
+	if request.method == "POST":
+		# get data from webpage
+		b = request.POST["buttonID"]
+		cmd = request.POST["cmd"]
+		cmdText = request.POST["cmdText"]
+		# override the selected buttons command
+		if b != "" and cmd != "" and cmdText != "":
+			instance = get_object_or_404(Button, id=int(b))
+			instance.command = cmd + " " + cmdText
+			instance.save()
+		else:
+			print("\nincomplete data\n")
+
 	return render(request=request,
 				  template_name="main/index.html",
 				  context={"buttons": Button.objects.all})
--- PiHost/mysite/urls.py
+++ PiHost/mysite/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    #path('', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include("main.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
