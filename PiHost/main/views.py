b0048c8033a6acc90a3e8fbc217f4d62aafd0319
NIklas Ziermann <N-Ziermann@protonmail.com>
2019-05-19 10:16:36 +0200

    Button Icons visible in webpanel

--- PiHost/main/views.py
+++ PiHost/main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Button
+
 # Create your views here.
 def homepage(request):
-	return HttpResponse("<b>hey</b>")
+	return render(request=request,
+				  template_name="main/index.html",
+				  context={"buttons": Button.objects.all})
