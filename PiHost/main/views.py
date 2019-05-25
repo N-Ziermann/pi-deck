e1d0bc11963b2e11a79cdd25781de4bac8f2c3b1
Niklas Ziermann <n-ziermann@protonmail.com>
2019-05-25 19:47:36 +0200

    Added image upload
    The web interface can now be used to change the button icons

--- PiHost/main/views.py
+++ PiHost/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Button
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def homepage(request):
@@ -9,10 +10,16 @@ def homepage(request):
 		b = request.POST["buttonID"]
 		cmd = request.POST["cmd"]
 		cmdText = request.POST["cmdText"]
+		#load the optional image upload
+		try:
+			img = request.FILES["img"]
+		except Exception as e:
+			img = None
 		# override the selected buttons command
 		if b != "" and cmd != "" and cmdText != "":
 			instance = get_object_or_404(Button, id=int(b))
 			instance.command = cmd + " " + cmdText
+			instance.img = img
 			instance.save()
 		else:
 			print("\nincomplete data\n")
