38303a43ae1fa2ada8407156f088e7e5c0d9546a
N-Ziermann <n-ziermann@protonmail.com>
2022-01-07 20:55:57 +0100

    Add basic electron app interface + foundation for webserver

---
+++ index.js
@@ -0,0 +1,60 @@
+const ks = require("node-key-sender");
+const { app, BrowserWindow, ipcMain, Tray, Menu } = require("electron");
+const path = require("path");
+const express = require("express");
+
+let expressApp = express();
+let expressPort = 3000;
+
+let mainWindow;
+let tray;
+let appStopped = false;
+
+const contextMenu = Menu.buildFromTemplate([
+  {
+    label: "Show",
+    click: () => mainWindow.show(),
+  },
+  {
+    label: "Hide",
+    click: () => mainWindow.hide(),
+  },
+  {
+    label: "Quit",
+    click: () => {
+      appStopped = true;
+      app.quit();
+    },
+  },
+]);
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  tray = new Tray(path.join(__dirname, "./react-icon.png"));
+  tray.setContextMenu(contextMenu);
+
+  mainWindow.on("close", (e) => {
+    if (!appStopped) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+  });
+
+  expressApp.get("/", (req, res) => {
+    return res.send("Received a GET HTTP method");
+  });
+  expressApp.listen(expressPort);
+});
+
+// ipcMain.on("typeText", (event, payload) => {
+//   setTimeout(() => {
+//     ks.sendText(payload.text);
+//   }, payload.delay);
+// });
---
+++ keyboard.js
@@ -0,0 +1,5 @@
+var ks = require('node-key-sender');
+
+setTimeout(async()=>{
+await ks.sendText('abcdefghijklmnopqrstuvwxyz')
+await ks.sendText('abcdefghijklmnopqrstuvwxyz')},2000)
